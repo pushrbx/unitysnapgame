8fc3bed942a835fa79f16818f7343e0841ae4d7a
pushrbx <thomace44@gmail.com>
2016-12-09 13:30:07 +0000

    initial commit

---
+++ Assets/Scripts/MainMenuViewModel.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+// ReSharper disable CheckNamespace
+
+public class MainMenuViewModel : MonoBehaviour
+{
+    public void OnStartButtonClicked()
+    {
+        SceneManager.LoadScene("Game", LoadSceneMode.Single);
+    }
+
+    public void OnSettingsButtonClicked()
+    {
+        //SceneManager.LoadScene("Settings", LoadSceneMode.Single);
+    }
+
+    public void OnExitButtonClicked()
+    {
+        Application.Quit();
+    }
+}
